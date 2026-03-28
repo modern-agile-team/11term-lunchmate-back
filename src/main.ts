96e282d54e406630174398ced97e9f7123bf3658
hawon6691 <hawonoj@outlook.kr>
2026-03-29 00:53:27 +0900

    feat: (#16) commons path rename

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { validationPipeOptions } from './common/validation/validation-pipe-options';
+import { validationPipeOptions } from './commons/validation/validation-pipe-options';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
