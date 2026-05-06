4e91106755dffef1497e2771bdb80f1bda58778e
임동영 <imdong207@gmail.com>
2026-05-06 17:51:04 +0900

    chore: (#151) 마이그레이션 설정 SSL 옵션 추가

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,4 +11,5 @@ export const AppDataSource = new DataSource({
   entities: [__dirname + '/../**/entities/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
   synchronize: false,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 });
